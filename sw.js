e6de4131460a24b8da95765bc3676d79feff4ec3
sliqadteam-beep <sliqad.team@gmail.com>
2026-08-19 21:53:28 +0200

    Refresh Sliqadius web cache for v16 repairs

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sliqadius-web-v15';
-const CORE=['./web.html','./web-v15.js','./sliq-i18n-loader.js','./logo-approved.jpg','./logo.png','./manifest.webmanifest'];
+const CACHE='sliqadius-web-v16';
+const CORE=['./web.html','./web-v15.js','./sliq-i18n-loader.js','./web-repair-v16.js','./logo-approved.jpg','./logo.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
