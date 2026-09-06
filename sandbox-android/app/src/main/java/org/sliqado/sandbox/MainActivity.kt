cb19926130b4422cfbb3781bbc569ff012b572d7
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-06 15:46:24 +0200

    Make Sand:Box Android native offline

--- sandbox-android/app/src/main/java/org/sliqado/sandbox/MainActivity.kt
+++ sandbox-android/app/src/main/java/org/sliqado/sandbox/MainActivity.kt
@@ -1,53 +1,22 @@
 package org.sliqado.sandbox
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.WindowManager
 
 class MainActivity : Activity() {
-    private lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        webView = WebView(this).apply {
-            setBackgroundColor(0xFF070908.toInt())
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.databaseEnabled = true
-            settings.useWideViewPort = true
-            settings.loadWithOverviewMode = true
-            settings.setSupportZoom(false)
-            settings.builtInZoomControls = false
-            settings.displayZoomControls = false
-            settings.allowFileAccess = false
-            settings.allowContentAccess = false
-            webChromeClient = WebChromeClient()
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                    val host = request.url.host ?: return true
-                    return if (host == "sliqado.org" || host.endsWith(".sliqado.org")) false else true
-                }
-            }
-            systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            loadUrl("https://sliqado.org/sandbox/")
-        }
-        setContentView(webView)
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
-    }
-
-    override fun onDestroy() {
-        webView.destroy()
-        super.onDestroy()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        )
+        setContentView(GameView(this))
     }
 }
