7e8c9ed2481b801d9ef7da1b554041fc06308853
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-12 11:38:15 +0200

    Add Qevyno Android v2 app config

---
+++ android/Qevyno/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "com.qevyno.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.qevyno.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0.0"
+    }
+    buildTypes { release { isMinifyEnabled = false } }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
