6af8d1f06bd5764a2702be851ff9374526625ca6
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-12 11:38:36 +0200

    Add Qevyno Android v2 activity

---
+++ android/Qevyno/app/src/main/java/com/qevyno/app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.qevyno.app;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle state) {
+        super.onCreate(state);
+
+        getWindow().setStatusBarColor(Color.rgb(10, 13, 18));
+        getWindow().setNavigationBarColor(Color.rgb(10, 13, 18));
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(10, 13, 18));
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowContentAccess(false);
+        s.setAllowFileAccess(true);
+        s.setAllowFileAccessFromFileURLs(true);
+        s.setAllowUniversalAccessFromFileURLs(true);
+        s.setMediaPlaybackRequiresUserGesture(true);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
