ef548af1ddd46ac7120bf721c57606b68fcf73eb
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-12 15:20:34 +0200

    Bump Qevyno to 2.9.2

--- android/Qevyno/app/build.gradle.kts
+++ android/Qevyno/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.qevyno.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.9.1"
+        versionCode = 14
+        versionName = "2.9.2"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
