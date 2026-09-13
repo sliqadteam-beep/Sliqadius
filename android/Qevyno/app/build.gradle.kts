cf02a2ccfe855b002e9d0b5a01278534e5d2a549
sliqadteam-beep <sliqad.team@gmail.com>
2026-09-13 10:26:55 +0200

    Bump Skaysa to 2.9.26

--- android/Qevyno/app/build.gradle.kts
+++ android/Qevyno/app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.qevyno.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "2.9.25"
+        versionCode = 38
+        versionName = "2.9.26"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
